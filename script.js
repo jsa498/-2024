4c0560daefb03b046b55cd18b581ea8545fda5a0
jsa498 <jaskaran1431085@gmail.com>
2024-10-17 23:40:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// If you want to add additional interactions like printing, this JavaScript is already set up for the button action.
+document.querySelector('.print-button').addEventListener('click', function() {
+    window.print();
+});
